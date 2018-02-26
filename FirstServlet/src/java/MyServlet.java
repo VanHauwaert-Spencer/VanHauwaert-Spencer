3f485c7d7ef9e7869da0c6336b53c9948a0e7feb
VanHauwaert-Spencer <hauwaert01@gmail.com>
2018-02-26 15:44:30 -0700

    Add files via upload

---
+++ FirstServlet/src/java/MyServlet.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+
+public class MyServlet extends HttpServlet {
+
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+         PrintWriter out = response.getWriter(); 
+            try {
+           
+            out.println("<h2>Welcome to my first servlet application for CIT 360</h2>");
+            
+        } finally {
+            out.close();
+        }
+    }
+
+}
